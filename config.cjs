6629397d687b4f6bcb32a25dde0e27e825a9aad1
jaansha <dkcj1470@gmail.com>
2024-10-11 16:50:59 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -20,7 +20,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "M J S",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "+94 71 345 7207",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "+94713457207",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : true, 
 };
